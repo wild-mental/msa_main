34d27292485a7afb01a96772c8238c5e8a5f8cd5
wild-mental <wild.mental@kidsnote.com>
2024-11-11 14:53:06 +0700

    initial commit

---
+++ src/main/java/hana/corp/msa_main/MsaMainApplication.java
@@ -0,0 +1,13 @@
+package hana.corp.msa_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hana/corp/msa_main/MsaMainApplicationTests.java
@@ -0,0 +1,13 @@
+package hana.corp.msa_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
