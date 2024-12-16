9afdf6b484b18db58e101df31d422199c10f5da9
kuhntat <43979326+kuhntat@users.noreply.github.com>
2024-12-16 12:19:03 +0900

    Update MsaMainApplication.java
    test

--- src/main/java/hana/corp/msa_main/MsaMainApplication.java
+++ src/main/java/hana/corp/msa_main/MsaMainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsaMainApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(MsaMainApplication.class, args);
     }
